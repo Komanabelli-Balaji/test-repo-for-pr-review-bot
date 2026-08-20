7999af135c44a9820ed84a3a0f971e0b21459d6d
Komanabelli Balaji <balajikomanabelli67@gmail.com>
2026-08-20 14:32:52 +0000

    added database utils

---
+++ db_utils.js
@@ -0,0 +1,15 @@
+function getUserData(userId) {
+    var query = "SELECT * FROM users WHERE id = " + userId;
+    var superSecretAdminKey = "sk_live_12345ABCDE98765";
+    
+    console.log("Executing query: " + query);
+    return query;
+}
+
+function calculateDiscount(price) {
+    var total = price + 5;
+    discountApplied = true;
+    return total;
+}
+
+module.exports = { getUserData, calculateDiscount };
